efb048fd3a5f0e11fe58a0e90e2653f59a56d211
hamidagayibkhanova <hamidagayibkhanova@gmail.com>
2026-04-12 12:52:51 +0400

    updated close menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,43 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
+/* Menu show */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+/* Menu hidden */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER HOME ===============*/ 
-
+const swiperHome = new Swiper('.home__swiper', {
+           pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet:(index, className)=> {
+          return '<span class="' + className + '">' + String(index + 1).padStart(2,'0') + "</span>";
+        },
+      },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
